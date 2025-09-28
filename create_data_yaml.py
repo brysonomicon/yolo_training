d492cbcf76ac148f9c05dbb23cc4fae782eeb169
LordWilliam <willjordan05@icloud.com>
2025-09-28 12:25:26 -0700

    WO: Adding some quality of life features to the create_data_yaml script. Perhaps I will rename it to training setup -- now it uses argparse to get the information it needs, it also creates the expected directory structure for model training.

--- create_data_yaml.py
+++ create_data_yaml.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import yaml
+import argparse
+import os
 
 '''
 Pass the path to obj.names to generate the data.yaml file for yolo training
@@ -33,6 +35,36 @@ def create_data_yaml(path):
     
     with open('data.yaml', 'w') as f:
         yaml.dump(dataset_config, f, default_flow_style=False)
+    
+    # Make the training directories (if they do not exist) 
+    directories = [
+        'dataset',
+        'dataset/images',
+        'dataset/images/train',
+        'dataset/images/val',
+        'dataset/images/test',
+        'dataset/labels',
+        'dataset/labels/train',
+        'dataset/labels/val',
+        'dataset/labels/test'
+    ]
+    
+    for directory in directories:
+        try:
+            os.mkdir(directory) 
+        except FileExistsError:
+            print(f'directory \'{directory}\' already exists')
+     
+    # Copy and split image / data pairs
+
 
 if __name__ == "__main__":
-    create_data_yaml('CVAT_AircraftCrashed_YoloAnnotations/obj.names')
+    parser = argparse.ArgumentParser(description="Performs YOLO model training setup")
+
+    parser.add_argument("--objnames-path", type=str, required=True, help='Path to the annotated data')
+    parser.add_argument("--images-path", type=str, required=False, help='Path to the training images')
+    parser.add_argument("--annotations-path", type=str, required=False, help="Path to the annotation files")
+
+    args = parser.parse_args()
+
+    create_data_yaml(args.objnames_path)
