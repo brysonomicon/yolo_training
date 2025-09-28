a06fea36327a2b0a0a22acbc64a7a01656b37048
LordWilliam <willjordan05@icloud.com>
2025-09-28 12:49:17 -0700

    WO: make a list of all the image label pairs.

--- create_data_yaml.py
+++ create_data_yaml.py
@@ -3,10 +3,19 @@ import yaml
 import argparse
 import os
 
-'''
-Pass the path to obj.names to generate the data.yaml file for yolo training
-'''
+class ImageLabelPair:
+    def __init__(self, path_to_image, path_to_label):
+        self.path_to_image = path_to_image
+        self.path_to_label = path_to_label
+        
+    def __str__(self):
+        return f'({self.path_to_image}, {self.path_to_label})'
+
+
 def create_data_yaml(path):     
+    '''
+    Pass the path to obj.names to generate the data.yaml file for yolo training
+    '''
     obj_names_path = Path(path)
     
     if not obj_names_path.exists():
@@ -58,16 +67,50 @@ def create_training_directories():
         except FileExistsError:
             print(f'directory \'{directory}\' already exists')
 
+def auto_split(path_to_labels, path_to_images):
+    '''
+    Splits training labels and images.
+    70,20,10; training, validation, test
+    '''
+    path_to_labels = Path(path_to_labels)
+    path_to_images = Path(path_to_images)
+   
+    bad_path = False
+    bad_path_message = ""
+    
+    if not path_to_images.exists() or not path_to_images.is_dir():
+        bad_path_message += f"training images directory does not exist or is not a directory,\n{path_to_labels}\n"
+        bad_path = True
+    if not path_to_labels.exists() or not path_to_labels.is_dir():
+        bad_path_message += f"training labels directory does not exist or is not a directory,\n{path_to_images}"
+        bad_path = True
+        
+    if (bad_path):
+        raise FileNotFoundError(bad_path_message)
+    
+    pairs = []
+    images = os.listdir(path_to_images)
+    images_list = ""
+    labels = os.listdir(path_to_labels)
+    labels_list = ""
+    
+    for image, label in images, labels:
+        pairs.append(ImageLabelPair(image, label))
+        
+    for imageLabel in pairs:
+        print(imageLabel)
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Performs YOLO model training setup")
 
     parser.add_argument('--objnames-path', type=str, required=True, help='Path to the annotated data')
-    parser.add_argument('--images-path', type=str, required=False, help='Path to the training images')
-    parser.add_argument('--annotations-path', type=str, required=False, help="Path to the annotation files")
+    parser.add_argument('--images-path', type=str, required=True, help='Path to the training images')
+    parser.add_argument('--annotations-path', type=str, required=True, help="Path to the annotation files")
 
     args = parser.parse_args()
 
     create_data_yaml(args.objnames_path)
     create_training_directories()
+    auto_split(args.annotations_path, args.images_path)
     
